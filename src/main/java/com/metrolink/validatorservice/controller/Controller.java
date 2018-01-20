f4251250a911a40c281e985f080e622ab7edc914
gabrielo91 <gabrielo_9103@hotmail.com>
2018-01-20 12:26:40 -0500

    Reflection implementada, llamado dinámico de metodos completamente funcional.

--- src/main/java/com/metrolink/validatorservice/controller/Controller.java
+++ src/main/java/com/metrolink/validatorservice/controller/Controller.java
@@ -5,7 +5,6 @@
  */
 package com.metrolink.validatorservice.controller;
 
-import com.metrolink.validatorservice.alarmsmanager.IAlarmsManager;
 import com.metrolink.validatorservice.bussinesvalidations.IValidations;
 import com.metrolink.validatorservice.db.daos.DAOLecturas;
 import com.metrolink.validatorservice.db.daos.IDAOLecturas;
@@ -14,16 +13,13 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
 
-
 /**
  *
  * @author Gabriel Ortega
  */
 public class Controller {
     
-    
     private IDAOLecturas daoLecturas;
-    private IAlarmsManager alarmsManager;
     private List<DTOLecturas> listaLecturasValidar; 
     private final IValidations validationsClass;
 
@@ -31,6 +27,14 @@ public class Controller {
         this.validationsClass = validationsClass;
     }
         
+    /**
+     * This method gets the un validated readings and performs validations over them
+     * @throws IllegalAccessException
+     * @throws IllegalArgumentException
+     * @throws InvocationTargetException
+     * @throws InstantiationException
+     * @throws NoSuchMethodException 
+     */
     public void performValidations() throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException, NoSuchMethodException{
         
         daoLecturas = new DAOLecturas();
@@ -39,16 +43,22 @@ public class Controller {
         for (int i = 0; i < listaLecturasValidar.size(); i++) {
             validateValue(i);
         }
-
     }
 
+    /**
+     * Iterate over each method declared in IValidations interface and uses it over an aeeayt of readings and a specific reading as well
+     * @param indexToValidate
+     * @throws IllegalAccessException
+     * @throws IllegalArgumentException
+     * @throws InvocationTargetException
+     * @throws InstantiationException
+     * @throws NoSuchMethodException 
+     */
     private void validateValue(int indexToValidate) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException, NoSuchMethodException {
-        
         Class validations = validationsClass.getClass();   
         
         for (Method bussinesValidation : IValidations.class.getMethods()) {
-            System.out.println("el nombre del metodo es: "+ bussinesValidation.getName());
-            System.out.println("el nombre del los argumentos son: " + bussinesValidation.getParameterCount());
+            System.out.println("La validacion a ejecutar es: "+ bussinesValidation.getName());
             Method validation = validations.getMethod(bussinesValidation.getName(), List.class, int.class);
             validation.invoke(validationsClass, listaLecturasValidar, indexToValidate);
         }
